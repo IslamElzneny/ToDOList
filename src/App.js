e7dd06dc1263ef782fb4bafc0d2d45842bfc4ed3
IslamElzneny <OmarHTI421@gmail.com>
2021-12-06 01:19:01 +0200

    modify the app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React ,{Component} from "react";
 import ToDoApp from "./components/ToDoList";
+import { BrowserRouter,Route } from "react-router-dom";
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <ToDoApp />
+        <BrowserRouter>
+          <Route path="/ToDoList" component={ToDoApp}/>
+        </BrowserRouter>
       </div>
     )
   }
